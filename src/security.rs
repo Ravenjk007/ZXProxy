0f4fa5a058bc3cb0361a4f02c79398a3bdfec7be
Ravenjk007 <pedrocloud51@gmail.com>
2026-07-18 17:39:04 -0300

    Rename handle function to handle_security and update logic

--- src/security.rs
+++ src/security.rs
@@ -3,8 +3,19 @@ use tokio::net::TcpStream;
 use anyhow::Result;
 use log::info;
 
-pub async fn handle(mut socket: TcpStream) -> Result<()> {
+pub async fn handle_security(mut socket: TcpStream) -> Result<()> {
     info!("🔐 SECURITY");
-    socket.write_all(b"SECURITY OK\n").await?;
+    
+    let mut buf = [0u8; 256];
+    let n = socket.read(&mut buf).await?;
+    let data = String::from_utf8_lossy(&buf[..n]);
+    info!("📩 SECURITY: {}", data);
+    
+    let response = "HTTP/1.1 200 OK\r\n\
+                    Connection: Upgrade\r\n\
+                    Upgrade: security\r\n\
+                    \r\n";
+    
+    socket.write_all(response.as_bytes()).await?;
     Ok(())
 }
