783ba2e151c4afa53d899641af7c707001dae17b
Ravenjk007 <pedrocloud51@gmail.com>
2026-07-18 15:46:56 -0300

    Rename and simplify handle_security function
    Refactor security handling function to simplify response.

--- src/security.rs
+++ src/security.rs
@@ -3,22 +3,8 @@ use tokio::net::TcpStream;
 use anyhow::Result;
 use log::info;
 
-pub async fn handle_security(mut socket: TcpStream) -> Result<()> {
-    info!("🔐 SECURITY handshake...");
-    
-    let mut buf = [0u8; 256];
-    let n = socket.read(&mut buf).await?;
-    let data = String::from_utf8_lossy(&buf[..n]);
-    
-    info!("📩 SECURITY: {}", data);
-    
-    let response = "HTTP/1.1 200 OK\r\n\
-                    Connection: Upgrade\r\n\
-                    Upgrade: security\r\n\
-                    \r\n";
-    
-    socket.write_all(response.as_bytes()).await?;
-    info!("🔐 SECURITY complete!");
-    
+pub async fn handle(mut socket: TcpStream) -> Result<()> {
+    info!("🔐 SECURITY");
+    socket.write_all(b"SECURITY OK\n").await?;
     Ok(())
 }
